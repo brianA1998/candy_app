055b498b1c58e126dabfe1ac36e9fb16c39ae332
Brian <brian.alaniz@hotmail.com>
2021-04-25 01:47:02 -0300

    Agregando fondo de pantalla de la app y agregando el logo de la app

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            height: 730,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/fondoApp.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: 20.0, top: 60.0, right: 20.0),
+                  width: 200.0,
+                  height: 200.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(1000.0),
+                      image: DecorationImage(
+                        image: AssetImage("assets/logoApp.jpeg"),
+                        fit: BoxFit.cover,
+                      )),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
