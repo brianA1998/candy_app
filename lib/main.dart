948a33421b59e984f1a9ac2fb3ebcd7ffd37f6c0
brianA1998 <brian.alaniz@hotmail.com>
2021-05-09 17:44:08 -0300

    Agregando dependecias de getX y poniendo GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'pages/login_page.dart';
+import 'package:get/route_manager.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: "Dulce delivery app",
-      routes: {
-        '/': (BuildContext context) => LoginPage(),
-      },
+      home: SplashPage(),
     );
   }
 }
